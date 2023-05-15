255ebb3ba6c4e8f58933c41a55f2a4e15413a366
Sean Montague <stmontague1@gmail.com>
2023-05-15 09:34:25 -0500

    Initial commit adding the starter code and pom packages

---
+++ src/main/java/com/messageapp/messageschedulerservice/MessageSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.messageapp.messageschedulerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSchedulerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSchedulerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messageapp/messageschedulerservice/MessageSchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messageapp.messageschedulerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSchedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
